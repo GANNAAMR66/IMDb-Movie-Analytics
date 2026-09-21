f9abbd407e4641c6fb7758e940ae314e417d67fe
Ganna Amr <ganna662006@gmail.com>
2026-09-21 23:08:07 +0300

    .

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.markdown("---")
 @st.cache_data
 def load_data():
     # IMPORTANT: Replace the URL below with your actual raw GitHub CSV link!
-    url = "https://raw.githubusercontent.com/GANNAAMR66/IMDb-Movie-Analytics/main/imdb_top_1000.csv"
+    url = "https://github.com/GANNAAMR66/IMDb-Movie-Analytics/blob/main/datasets/imdb_clean6.csv"
     try:
         return pd.read_csv(url)
     except Exception as e:
